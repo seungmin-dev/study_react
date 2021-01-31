12a6d4ca3f489bb97c664b0bcf9015fed61dc7ad
seungmin_dev <seungmin.dev@gmail.com>
2021-02-01 05:39:41 +0900

    #1.1 Arrow Functions

---
+++ #1. Fundamentals/#1.1 Arrow Functions.js
@@ -0,0 +1,12 @@
+function sayHello(name = "Human") { //함수에서 파라미터 값의 default를 지정할 수 있음
+    //name의 값이 없으면 Human으로
+    //name의 값이 지정되어서 들어오면 그 값으로 출력됨
+    return "Hello " + name;
+}
+// 위의 함수와 동일한 코드
+const sayHello = (name = "Human") => "Hello " + name;
+// arrow function은 이벤트를 추가하거나 익명함수를 만들 때 용이함
+
+const seungmin = sayHello();
+
+console.log(seungmin);
